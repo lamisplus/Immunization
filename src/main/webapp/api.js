2d2a0f2e81417f2637550bd66c8be04d6e10ae82
Emmanuel Chinonye Nnajiofor <emma.nnajichi@gmail.com>
2024-02-20 19:15:38 +0100

    minor changes to achieve schema for routine immunization table

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,5 +2,5 @@ export const url = process.env.NODE_ENV === "development"
   ? "http://localhost:8383/api/v1/"
   : "/api/v1/";
 export const token = process.env.NODE_ENV === "development"
-  ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3ODU4NzUyfQ.Kwp5JlGxoRe9_iE6qPAR2SaQFAj9vbnHlQazjZMrwN-FF_2w1MCbaDCHjAPGaXbqW0TDDybZk-EIzWuZqkYgYA"
+  ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4MTAxMjkzfQ.DifHMd9ULaosBR_QWA4mJ9fb76B2D-1m3QmF-GXrNJ7YxOgOjvFrgPiJ0MMlnf_7F8d6JhFmBa7M4Oe0mw8FwA"
   : new URLSearchParams(window.location.search).get("jwt");
