eab74b9723f03fa02a1506f180c78a0792794263
Emmanuel Chinonye Nnajiofor <emma.nnajichi@gmail.com>
2024-03-12 16:05:33 +0100

    implementing documented corrections

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,5 +4,5 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEwMDk3NjI1fQ.baueWIv586roBbtlD8awc5l_XFdqB8dhxK0DwjxA4eqWg5Uz5lajw01KkaeKXBRlGhmEDzfCNUsGLWtY21rzlw"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzEwMjgzNTEwfQ.fte0j1CQx9Pq-HsoIFFeFTHiinm4Xr_mFMpFE6c8NSpk30UgAqDZVspE5L1Si6zxe4krdlnmMSRc3lNWZl6qew"
     : new URLSearchParams(window.location.search).get("jwt");
